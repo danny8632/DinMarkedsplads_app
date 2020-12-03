4546b0dbaa390bd74098d049a4ea98e1b36ddf9e
Danny Haslund <dannyhasl1@gmail.com>
2020-12-03 19:15:30 +0100

    more init

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Gemmedawdawaw</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
